e2945363e308c284dac4fe241a215678b218568d
Will McGinty <mcgintw@gmail.com>
2022-12-19 21:39:57 -0600

    better day 16

--- Day16/Day16/main.swift
+++ Day16/Day16/main.swift
@@ -8,106 +8,110 @@
 import Foundation
 import AdventKit
 import Parsing
-import Collections
 
-struct Valve: Equatable {
+struct Valve: Hashable {
     let name: String
     let flowRate: Int
     let connectedValveNames: [String]
 }
 
-struct State: Hashable {
-    var flowReleased: Int
-    var current: String
-    var opened: Set<String>
-    var otherPeople: Int
-    var time: Int
+struct IndexedValve: Hashable {
+    let index: Int
+    let valve: Valve
+    
+    var name: String { valve.name }
+    var flowRate: Int { valve.flowRate }
+    var connectedValveNames: [String] { valve.connectedValveNames }
 }
+
 
 let valveParser = Parse {
     "Valve "
-
     Prefix(while: { $0 != " " })
-
     " has flow rate="
     Int.parser()
-
     OneOf {
         "; tunnels lead to valves "
         "; tunnel leads to valve "
     }
-
     Many {
         Prefix(while: { $0 != "," && $0 != "\n" })
     } separator: { ", " }
-}.map {
-    Valve(name: String($0), flowRate: $1, connectedValveNames: $2.map(String.init))
-}
+}.map { Valve(name: String($0), flowRate: $1, connectedValveNames: $2.map(String.init)) }
 
 let valvesParser = Many { valveParser } separator: { "\n" }
 
-func maximumFlowReleased(using valves: [String: Valve], inTotalTime totalTime: Int, withOtherPeople count: Int) -> Int {
-    var best = 0
-    let start = State(flowReleased: 0, current: "AA", opened: [], otherPeople: count, time: totalTime)
-    var deque = Deque([start])
-    var seen = Set<State>()
-
-    let allFlowingValves = Set(valves.values.filter { $0.flowRate > 0 }.map(\.name))
-
-    while let status = deque.popFirst() {
-        best = max(best, status.flowReleased)
-
-        if status.time == 0 {
-            // if we've got no time left, but we do have players left, pass it on
-            guard status.otherPeople > 0 else { continue }
-            deque.append(.init(flowReleased: status.flowReleased, current: "AA", opened: status.opened, otherPeople: status.otherPeople - 1, time: totalTime))
-        }
-
-        if status.opened == allFlowingValves {
-            continue
-        }
-
-        if seen.contains(status) {
-            continue
-        }
-
-        seen.insert(status)
-        let currentValve = valves[status.current]!
-
-        // if we have not visited this valve, and it will help to open it, lets open it
-        if currentValve.flowRate > 0 && !status.opened.contains(currentValve.name){
-            deque.append(State(flowReleased: status.flowReleased + ((status.time - 1) * (currentValve.flowRate)), current: status.current,
-                               opened: status.opened.union([status.current]), otherPeople: status.otherPeople, time: status.time - 1))
-        }
-
-        // travel to each neighbor
-        if status.time >= 1 {
-            for neighbor in currentValve.connectedValveNames {
-                deque.append(.init(flowReleased: status.flowReleased, current: neighbor, opened: status.opened, otherPeople: status.otherPeople, time: status.time - 1))
-            }
-        }
+class Solver {
+    
+    struct Key: Hashable {
+        let position: Int
+        let opened: Set<Int>
+        let timeRemaining: Int
+        let otherPlayers: Int
     }
 
-    return best
+    let valveDict: [String: IndexedValve]
+    private var cache: [Key: Int] = [:]
+    
+    init(valveDict: [String: IndexedValve]) {
+        self.valveDict = valveDict
+    }
+
+    /* What is the maximum score I can achieve when I have am [valveName], after opening [opened] vales,
+     with [timeRemaining] time remainining our of [maximumTime], with [otherPlayers] left to run. */
+    func maxFlowReleasable(at valveName: String, afterOpening opened: Set<IndexedValve>,
+                           timeRemaining: Int, maximumTime: Int, otherPlayers: Int) -> Int {
+        if timeRemaining == 0 {
+            // If we have no time left, but other players left -> start running them
+            return otherPlayers > 0 ? maxFlowReleasable(at: "AA", afterOpening: opened,
+                                                        timeRemaining: maximumTime, maximumTime: maximumTime, otherPlayers: otherPlayers - 1) : 0
+        }
+        
+        let valve = valveDict[valveName]!
+        let key = Key(position: valve.index, opened: Set(opened.map(\.index)), timeRemaining: timeRemaining, otherPlayers: otherPlayers)
+        if let cached = cache[key] {
+            // If we've previously seen this position, we already know the max score achievable from here
+            return cached
+        }
+        
+        var answer: Int = 0
+        
+        //if we haven't opened this valve, and we should, open it
+        if !opened.contains(valve) && valve.flowRate > 0 {
+            let futureFlow = ((timeRemaining - 1) * valve.flowRate) + maxFlowReleasable(at: valve.name, afterOpening: opened.union([valve]),
+                                                                                        timeRemaining: timeRemaining - 1, maximumTime: maximumTime, otherPlayers: otherPlayers)
+            answer = max(answer, futureFlow)
+        }
+        
+        // traverse the neighbors
+        for neighbor in valve.connectedValveNames {
+            answer = max(answer, maxFlowReleasable(at: neighbor, afterOpening: opened, timeRemaining: timeRemaining - 1, maximumTime: maximumTime, otherPlayers: otherPlayers))
+        }
+        
+        cache[key] = answer
+        return answer
+    }
 }
 
-//// MARK: - Part 1
+// MARK: - Part 1
 func maximumFlowReleasedbySelf(from input: String) throws -> Int {
-    let valves = try valvesParser.parse(input)
-    let maxFlow = maximumFlowReleased(using: Dictionary(uniqueKeysWithValues: valves.map { ($0.name, $0) }), inTotalTime: 30, withOtherPeople: 0)
-    return maxFlow
+    let valves = try valvesParser.parse(input).enumerated().map { IndexedValve(index: $0.offset, valve: $0.element) }
+    
+    let solver = Solver(valveDict: Dictionary(uniqueKeysWithValues: valves.map { ($0.name, $0) }))
+    return solver.maxFlowReleasable(at: "AA", afterOpening: [], timeRemaining: 30, maximumTime: 30, otherPlayers: 0)
 }
 
 try measure(part: .one) {
-    print("Solution: \(try maximumFlowReleasedbySelf(from: .testInput))")
+    print("Solution: \(try maximumFlowReleasedbySelf(from: .input))")
 }
 
 // MARK: - Part 2
-// Warning: It takes a really long time. Could/should/definitely be optimized.
+// Warning: It takes ~6.5 minutes. But it WORKS.
 func maximumFlowReleasedWithElephant(from input: String) throws -> Int {
-    let valves = try valvesParser.parse(input)
-    let maxFlow = maximumFlowReleased(using: Dictionary(uniqueKeysWithValues: valves.map { ($0.name, $0) }), inTotalTime: 26, withOtherPeople: 1)
-    return maxFlow
+    let valves = try valvesParser.parse(input).enumerated().map { IndexedValve(index: $0.offset, valve: $0.element) }
+    
+    let solver = Solver(valveDict: Dictionary(uniqueKeysWithValues: valves.map { ($0.name, $0) }))
+    return solver.maxFlowReleasable(at: "AA", afterOpening: [], timeRemaining: 26, maximumTime: 26, otherPlayers: 1)
 }
 
 try measure(part: .two) {
